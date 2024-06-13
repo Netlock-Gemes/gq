a9474bcec13b4ff3e1143c8a81aa063b6536dce8
H-R-Wells2 <hrwells10246@gmail.com>
2024-06-13 14:59:05 +0530

    fixed login bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,6 @@ const Navbar = () => {
     }, []);
 
     const handleMenuClick = (menuOption) => {
-        console.log(`Clicked ${menuOption}`);
         setIsDropdownOpen(false);
     };
 
--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import authContext from './authContext'
-
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authContext from './authContext';
+import { toast } from 'react-toastify';
 
 const AuthState = (props) => {
-
-
     const [isLogin, setIsLogin] = useState(false);
-
-    const navigate = useNavigate();
-
     const [loggedInUserData, setLoggedInUserData] = useState([]);
+    const navigate = useNavigate();
 
     const getLoggedInUserData = async () => {
         const token = localStorage.getItem('token');
@@ -20,39 +15,49 @@ const AuthState = (props) => {
             return;
         }
 
-        const response = await fetch('http://localhost:5000/api/auth/profile', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-auth-token': token,
-            },
-        });
-        const data = await response.json();
-        if (response.ok) {
-            setIsLogin(true);
-            setLoggedInUserData(data);
-        } else {
-            // toast.error(data.msg || "Failed to fetch profile data", {
-            //     autoClose: 2000,
-            // });
-            console.log(data)
-            // navigate('/login');
+        try {
+            const response = await fetch('http://localhost:5000/api/auth/profile', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-auth-token': token,
+                },
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setIsLogin(true);
+                setLoggedInUserData(data);
+            } else {
+                toast.error(data.msg || "Failed to fetch profile data", {
+                    autoClose: 2000,
+                });
+                console.log(data);
+                navigate('/login');
+            }
+        } catch (error) {
+            console.error('Error fetching profile data:', error);
+            toast.error('An error occurred while fetching profile data', {
+                autoClose: 2000,
+            });
+            navigate('/login');
         }
     };
 
-    const checkLogin = () => {
-        getLoggedInUserData().then(() => {
-            if (!isLogin) {
-                navigate('/login');
-            }
+    const checkLogin = async () => {
+        await getLoggedInUserData();
+        if (!isLogin) {
+            console.log(isLogin);
+            // navigate('/login');
         }
-    )}
+    };
 
     return (
         <authContext.Provider value={{ checkLogin, getLoggedInUserData, loggedInUserData, setLoggedInUserData, isLogin, setIsLogin }}>
             {props.children}
         </authContext.Provider>
-    )
-}
+    );
+};
 
-export default AuthState
+export default AuthState;
